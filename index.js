54ccd7e89fdacb9511597590e0d8e03fe9e26fcf
LongarSoft <longarsoft@gmail.com>
2024-05-17 16:13:24 -0400

    Solucionado problema git

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const header = document.querySelector('header');
 const main = document.querySelector('main');
 let lastScrollY = window.scrollY;
@@ -30,31 +29,3 @@ window.addEventListener('scroll', function() {
         ticking = true;
     }
 });
-=======
-const header = document.querySelector('header');
-const main = document.querySelector('main');
-
-// Agrega un evento de desplazamiento a la ventana
-window.addEventListener('scroll', function() {
-    // Verifica si el desplazamiento vertical es mayor a 50px desde la parte superior
-    if (window.scrollY > 60) {
-        // Si es así, agrega la clase 'scrolled' al encabezado
-        header.classList.add('scrolled');
-    } else {
-        // Si no, elimina la clase 'scrolled'
-        header.classList.remove('scrolled');
-    }
-});
-
-// Agrega un evento de desplazamiento a la ventana
-window.addEventListener('scroll', function() {
-    // Verifica si el desplazamiento vertical es mayor a 50px desde la parte superior
-    if (window.scrollY > 60) {
-        // Si es así, agrega la clase 'scrolled' a main
-        main.classList.add('scrolled-padding');
-    } else {
-        // Si no, elimina la clase 'scrolled-padding'
-        main.classList.remove('scrolled-padding');
-    }
-});
->>>>>>> ef4521ba240401a43d816f16f23105539b84427d
